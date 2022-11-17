9d3c3f73e1941dbbf378bf9b0d9b8fbb65a0b017
Abdullah Najid <abdullah.najid16@gmail.com>
2022-11-17 00:39:38 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeArr) {
+  let newArr = [];
 
+  for (const i of arr) {
+    if (!removeArr.includes(i)) {
+      newArr.push(i);
+    }
+  }
+
+  return newArr;
 };
 
 // Do not edit below this line

ee4d490db0908916e5896adf4fe498ac46fd13d9
Abdullah Najid <abdullah.najid16@gmail.com>
2022-11-16 23:26:43 -0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0)
+    return 'ERROR';
+  
+  let output = '';
+  for (let i = 0; i < num; i++) {
+    output += str;
+  }
+  console.log(output);
+  return output;
 };
 
 // Do not edit below this line

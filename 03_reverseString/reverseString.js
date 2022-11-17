69af720134bbeda8f055959c650e6bb94098869d
Abdullah Najid <abdullah.najid16@gmail.com>
2022-11-16 23:42:28 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let reverseStr = '';
+  for (let i = str.length; i >= 0; i--)
+    reverseStr += str.charAt(i);
 
+  console.log(reverseStr);
+  return reverseStr;
 };
 
 // Do not edit below this line

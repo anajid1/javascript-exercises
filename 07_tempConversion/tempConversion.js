a42be698e0a5bb31dec5361120921f1fe92edcb0
Abdullah Najid <abdullah.najid16@gmail.com>
2022-11-17 12:36:23 -0500

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let c = (f - 32) * (5 / 9);
+  let cRounded = Math.round(c * 10) / 10;
 
+  return cRounded;
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let f = c * (9 / 5) + 32;
+  let fRounded = Math.round(f * 10) / 10;
 
+  return fRounded;
 };
 
 // Do not edit below this line

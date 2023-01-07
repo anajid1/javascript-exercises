06c7fc3c3c2cfc5fd0e82ba6f9bf3c931157e5a4
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2023-01-07 18:08:24 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	var sum = 0;
+  for (const number of numbers)
+    sum += number;
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  if (numbers.length === 0) 
+    return 0;
 
+  var sum = 1;
+  for (const number of numbers)
+    sum *= number;
+  
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 1 || num === 0)
+    return 1;
+  else
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line

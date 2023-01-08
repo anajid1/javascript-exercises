3ecc888a6f65d02b3334c320916364f9afe7ad0c
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2023-01-07 22:28:36 -0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+  if (fib < 0)
+    return "OOPS";
+
+  if (fib == 0)
+    return 0;
+
+  if (fib == 1 || fib == 2)
+    return 1;
+  
+  var prev2 = 1;
+  var prev1 = 1;
+
+  var curr;
+
+  for (var i = 3; i <= fib; i++) {
+    curr = prev2 + prev1;
+
+    prev2 = prev1;
+    prev1 = curr;
+  }
+
+  return curr;
 
 };
 
